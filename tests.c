843a930bff997aa701be701fea5bf759e243916e
9jat <hamidov9jat@gmail.com>
2022-05-13 00:56:03 +0400

    "working test for read_book function"

--- tests.c
+++ tests.c
@@ -1,4 +1,4 @@
-#define  _GNU_SOURCE
+#define _GNU_SOURCE
 #include "queue.h"
 #include "haiku.h"
 #include <stdio.h>
@@ -7,6 +7,30 @@
 #include <string.h>
 
 #include <CUnit/Basic.h>
+
+bool compare_haikus(haiku h1, haiku h2)
+{
+    if (strcmp(h1.author, h2.author))
+        return false;
+    for (int i = 0; i < 3; i++)
+        if (strcmp(h1.lines[i], h2.lines[i]))
+        {
+            return false;
+        }
+    return true;
+}
+
+bool compare_books(book b1, book b2)
+{
+    if (b1.size != b2.size)
+        return false;
+    for (int i = 0; i < b1.size; i++)
+        if (!compare_haikus(b1.poems[i], b2.poems[i]))
+        {
+            return false;
+        }
+    return true;
+}
 
 /* Pointer to the file used by the tests. */
 static FILE *temp_file = NULL;
@@ -44,15 +68,14 @@ int clean_suite1(void)
     }
 }
 
-void testMsgQueue(void){
+void testMsgQueue(void)
+{
 
     int test_id_queue;
 
-    CU_ASSERT( -1 != (test_id_queue = create_queue()) );
-    CU_ASSERT( -1 != (test_id_queue = access_queue()) );
-    CU_ASSERT( -1 != (remove_queue(test_id_queue)) );
-    
-
+    CU_ASSERT(-1 != (test_id_queue = create_queue()));
+    CU_ASSERT(-1 != (test_id_queue = access_queue()));
+    CU_ASSERT(-1 != (remove_queue(test_id_queue)));
 }
 
 void testGetline(void)
@@ -66,35 +89,29 @@ void testGetline(void)
         fprintf(temp_file, "testing_getline\n");
         rewind(temp_file); // set the file pointer at the beginning of the stream
 
-        CU_ASSERT( number_of_characters == (getline(&line, &len, temp_file)) ); 
+        CU_ASSERT(number_of_characters == (getline(&line, &len, temp_file)));
         // test against number of characters in the first
         // line of temp_file (i.e testing_getline\n) excluding NULL character
     }
 }
 
-bool compare_haikus(haiku h1, haiku h2)
+void testReadBook(void)
 {
-    if (strcmp(h1.author, h2.author))
-        return false;
-    for (int i = 0; i < 3; i++)
-        if (strcmp(h1.lines[i], h2.lines[i]))
-            return false;
-    return true;
+    haiku six_haikus[6] = {
+        {"Matsuo Basho (1644-1694)", {"Autumn moonlight –", "a worm digs silently", "into the chestnut."}},
+        {"Matsuo Basho (1644-1694)", {"Old pond", "a frog jumps", "the sound of water"}},
+        {"Kobayashi Issa (1763-1828)", {"Autumn wind –", "mountain’s shadow", "wavers."}},
+        {"Kobayashi Issa (1763-1828)", {"Don’t weep, insects –", "Lovers, stars themselves,", "Must part"}},
+        {"Masaoka Shiki (1867-1902)", {"In the coolness", "of the empty sixth-month sky...", "the cuckoo’s cry."}},
+        {"Masaoka Shiki (1867-1902)", {"the tree cut,", "dawn breaks early", "at my little window"}}};
+
+    book test_haiku_book = {6, six_haikus};
+
+    CU_ASSERT_TRUE(compare_books(read_book(japanese), test_haiku_book));
 }
 
-bool compare_books(book b1, book b2)
+void test()
 {
-    if (b1.size != b2.size)
-        return false;
-    for (int i = 0; i < b1.size; i++)
-        if (!compare_haikus(b1.poems[i], b2.poems[i]))
-            return false;
-    return true;
-}
-
-
-
-void test(){
     book b;
     b.size = 2;
     b.poems = calloc(2, sizeof(haiku));
@@ -118,6 +135,7 @@ void test(){
  */
 int main()
 {
+
     CU_pSuite pSuite = NULL;
 
     /* initialize the CUnit test registry */
@@ -134,8 +152,8 @@ int main()
 
     /* add the tests to the suite */
     if (NULL == CU_add_test(pSuite, "test of testGetline", testGetline) ||
-        NULL == CU_add_test(pSuite, "testing msg queue functions", testMsgQueue)
-    )
+        NULL == CU_add_test(pSuite, "testing msg queue function", testMsgQueue) ||
+        NULL == CU_add_test(pSuite, "testing read_book function", testReadBook))
     {
         CU_cleanup_registry();
         return CU_get_error();
