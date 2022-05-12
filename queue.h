cb0c04d381ff3a9dde4afeea02775ee88855eda8
9jat <hamidov9jat@gmail.com>
2022-05-12 23:42:37 +0400

    "working tests for msgqueue"

--- queue.h
+++ queue.h
@@ -10,7 +10,7 @@ struct haiku_msg{
 
 int create_queue();
 int access_queue();
-void remove_queue(int id);
+int remove_queue(int id);
 int write_haiku(category c, haiku *h);
 int read_haiku(category c, haiku *h);
 
